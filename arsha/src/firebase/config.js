148db222bb2e0872b9a3ff6552f25acb0fc02925
samuel1212703 <37654743+samuel1212703@users.noreply.github.com>
2021-11-10 19:41:42 +0100

    works, still

--- arsha/src/firebase/config.js
+++ arsha/src/firebase/config.js
@@ -1,17 +1,22 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, getAdditionalUserInfo } from "firebase/auth";
-import { getFirestore, collection, doc, setDoc } from "firebase/firestore";
-import * as admin from "firebase-admin";
-import { async } from "@firebase/util";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import {
+	getFirestore,
+	collection,
+	doc,
+	setDoc,
+	getDoc,
+} from "firebase/firestore";
+import slugify from "slugify";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -21,44 +26,45 @@ export const provider = new GoogleAuthProvider();
 const db = getFirestore();
 
 export async function createReview(title, creator, rating) {
-  const userID = auth.currentUser.uid;
-  const reveiwDoc = doc(
-    collection(doc(collection(db, "users"), userID), "reviews"),
-    title
-  );
-  const data = {
-    userID: userID,
-    title: title,
-    creator: creator,
-    rating: rating,
-    creationDate: new Date(),
-  };
-  await setDoc(reveiwDoc, data);
+	const userID = auth.currentUser.uid;
+	const reveiwDoc = doc(
+		collection(doc(collection(db, "users"), userID), "reviews"),
+		slugify(title).toLowerCase()
+	);
+	const data = {
+		userNum: await getCounter(),
+		userID: userID,
+		title: title,
+		creator: creator,
+		rating: rating,
+		creationDate: new Date(),
+		slug: slugify(title).toLowerCase(),
+	};
+	await setDoc(reveiwDoc, data);
+	await incrementCounter();
 }
 
-export async function storeNewUser(){
-	console.log(auth);
-	console.log(getAdditionalUserInfo(auth.currentUser.uid)["_tokenresponse"].isNewUser);
+export async function storeNewUser() {
+	//console.log(auth);
+	//console.log(getAdditionalUserInfo(auth.currentUser.uid)["_tokenresponse"].isNewUser);
 }
 
-export async function getUsers() {
-  const listAllUsers = (nextPageToken) => {
-    // List batch of users, 1000 at a time.
-    getAuth()
-      .listUsers(1000, nextPageToken)
-      .then((listUsersResult) => {
-        listUsersResult.users.forEach((userRecord) => {
-          console.log("user", userRecord.toJSON());
-        });
-        if (listUsersResult.pageToken) {
-          // List next batch of users.
-          listAllUsers(listUsersResult.pageToken);
-        }
-      })
-      .catch((error) => {
-        console.log("Error listing users:", error);
-      });
-  };
-  // Start listing users from the beginning, 1000 at a time.
-  return listAllUsers();
+export async function getCounter() {
+	const infoRef = doc(collection(db, "users"), "info");
+	const infoSnap = await getDoc(infoRef);
+	if (!infoSnap.exists()) {
+		console.log("No info document!");
+	}
+	return infoSnap.data().counter;
 }
+
+export async function incrementCounter() {
+	const infoRef = doc(collection(db, "users"), "info");
+	const infoSnap = await getDoc(infoRef);
+	if (!infoSnap.exists()) {
+		console.log("No info document!");
+	}
+	setDoc(infoRef, { counter: infoSnap.data().counter++ });
+}
+
+export async function getUsers() {}
