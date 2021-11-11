c6e5adaacf6ba20f1a3546347cdefb6838b74b29
samuel1212703 <37654743+samuel1212703@users.noreply.github.com>
2021-11-11 20:52:13 +0100

    y

--- arsha/src/App.js
+++ arsha/src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
 import React, { useState } from "react";
 import { signInWithPopup, signOut } from "firebase/auth";
-import { auth, provider, createReview, storeNewUser } from "./firebase/config";
+import {
+  auth,
+  provider,
+  createReview,
+  storeNewUser,
+  getUserReviews,
+  storeImage,
+} from "./firebase/config";
 import GoogleButton from "react-google-button";
 import axios from "axios";
+//import FuzzySearch from "react-fuzzy";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 function signinPopup(auth, provider, setLoggedIn) {
   signInWithPopup(auth, provider)
@@ -30,10 +40,80 @@ function signout(auth, setLoggedIn) {
     });
 }
 
-function autoFill(setTitle, setSelectedImage) {
-  setTitle(this.state.suggestionList.title);
-  setSelectedImage(this.state.suggestionList.image);
+function autoFill(setTitle, setSelectedImage, i) {
+  setTitle(suggestionList[i].title);
+  setSelectedImage(suggestionList[i].image);
 }
+
+async function setSuggestionList(searchQuery) {
+  const movie_api_key = "01096f6ebe34f8db1ce6adc0d4ab0e10";
+  const movie_image_url = "https://image.tmdb.org/t/p/w500";
+  const url =
+    "https://api.themoviedb.org/3/search/movie?api_key=" +
+    movie_api_key +
+    "&language=en-US&query=" +
+    searchQuery.replace(" ", "%20") +
+    "&page=1&include_adult=true";
+  return axios.get(url).then(function (response) {
+    const data = response.data.results;
+    const suggestionUnformatted = [];
+    for (var i = 0; i < 3; i++) {
+      suggestionUnformatted.push({
+        title: data[i].title,
+        image: movie_image_url + data[i].poster_path,
+        rating: data[i].vote_average,
+      });
+    }
+    return Object.keys(suggestionUnformatted).sort(function (a, b) {
+      return suggestionUnformatted.rating[a] - suggestionUnformatted.rating[b];
+    });
+  });
+}
+
+var suggestionList = [
+  {
+    id: 1,
+    title: "",
+    author: "",
+    image: "",
+    rating: "",
+  },
+  {
+    id: 2,
+    title: "",
+    author: "",
+    image: "",
+    rating: "",
+  },
+  {
+    id: 3,
+    title: "",
+    author: "",
+    image: "",
+    rating: "",
+  },
+];
+
+var displayReviews = [
+  {
+    image: "https://image.tmdb.org/t/p/w500/k7uOoGUk1JiN3gfKefUj2kP3Vu6.jpg",
+    title: "Vampire Hunter D.",
+    reviewer: "Samuel Jakobsen",
+    rating: "unknown",
+  },
+  {
+    image: "https://image.tmdb.org/t/p/w500/k7uOoGUk1JiN3gfKefUj2kP3Vu6.jpg",
+    title: "Vampire Hunter D.",
+    reviewer: "Samuel Jakobsen",
+    rating: "unknown",
+  },
+  {
+    image: "https://image.tmdb.org/t/p/w500/k7uOoGUk1JiN3gfKefUj2kP3Vu6.jpg",
+    title: "Vampire Hunter D.",
+    reviewer: "Samuel Jakobsen",
+    rating: "unknown",
+  },
+];
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -41,39 +121,27 @@ function App() {
   const [title, setTitle] = useState("");
   const [creator, setCreator] = useState("");
   const [rating, setRating] = useState(0);
-  this.state = [{}];
-  const [searchQuery, setSearchQuery] = useState("");
+  const [medium, setMedium] = useState("");
 
-  // Search Query
-  if (searchQuery.length > 3) {
-    const movie_api_key = "01096f6ebe34f8db1ce6adc0d4ab0e10";
-    const movie_image_url = "https://image.tmdb.org/t/p/w500";
-    const url =
-      "https://api.themoviedb.org/3/search/movie?api_key=" +
-      movie_api_key +
-      "&language=en-US&query=" +
-      searchQuery.replace(" ", "%20") +
-      "&page=1&include_adult=true";
-    axios.get(url).then(function (response) {
-      const data = response.data.results;
-      const suggestionUnformatted = [];
-      for (var i = 0; i < 3; i++) {
-        suggestionUnformatted.push({
-          title: data[i].title,
-          image: movie_image_url + data[i].poster_path,
-          rating: data[i].vote_average,
-        });
-      }
-      const suggestionFormatted = Object.keys(suggestionUnformatted).sort(
-        function (a, b) {
-          return (
-            suggestionUnformatted.rating[a] - suggestionUnformatted.rating[b]
-          );
-        }
-      );
-      this.setState({ suggestionList: suggestionFormatted });
-    });
-  }
+  const maxImageSizeBytes = 5000000;
+
+  getUserReviews().then((product) => {
+    if (product[0]) {
+      displayReviews[0] = product[0];
+    }
+    if (product[1]) {
+      displayReviews[1] = product[1];
+    }
+    if (product[2]) {
+      displayReviews[2] = product[2];
+    }
+  });
+
+  // // Search Query
+  // const [searchQuery, setSearchQuery] = useState("");
+  // if (searchQuery.length > 0) {
+  //   suggestionList = setSuggestionList(searchQuery);
+  // }
 
   return (
     <div className="App">
@@ -90,19 +158,6 @@ function App() {
         />
       )}
       <div className="mainPage">
-        <div className="sideBar">
-          <input
-            id="searchBar"
-            type="text"
-            placeholder="Enter user name or id"
-          />
-          <button id="searchButton">
-            <img id="searchImage" src="images/search-icon.png" />
-          </button>
-          {/* getUsers().map((user) => {
-            <div>{user.name}</div>;
-          }) */}
-        </div>
         <div className="mainContent">
           <div id="reviewImageContainer">
             <div>
@@ -128,7 +183,11 @@ function App() {
           </div>
           <div id="reviewTextContainer">
             <p>Medium</p>
-            <select name="mediums" id="mediums">
+            <select
+              name="mediums"
+              id="mediums"
+              onChange={(event) => setMedium(event.target.value)}
+            >
               <option value="Album">Album</option>
               <option value="Song">Song</option>
               <option value="Movie">Movie</option>
@@ -137,14 +196,49 @@ function App() {
               <option value="Actor">Actor</option>
             </select>
             <p>Title</p>
+            <div>
+              {/* <FuzzySearch
+              list={suggestionList}
+              keys={["author", "title"]}
+              width={430}
+              onChange={(event) => {
+                setSearchQuery(event.target.value);
+                console.log(searchQuery);
+              }}
+              onSelect={(newSelectedItem) => {
+                autoFill(setTitle, setSelectedImage, newSelectedItem.id--);
+              }}
+              resultsTemplate={(props, state, styles, clickHandler) => {
+                return state.results.map((val, i) => {
+                  const style = {
+                    backgroundColor: "white",
+                  };
+                  const imageStyle = {
+                    width: "64px",
+                    height: "64px",
+                  };
+                  return (
+                    <div key={i} style={style} onClick={() => clickHandler(i)}>
+                      <img src={val.image} style={imageStyle}></img>
+                      {val.title}
+                      <span style={{ float: "right", opacity: 0.5 }}>
+                        by {val.author}
+                      </span>
+                    </div>
+                  );
+                });
+              }}
+            /> */}
+            </div>
             <input
               type="text"
               id="title"
               name="title"
               placeholder="Blonde"
+              value={title}
               onChange={(event) => {
                 setTitle(event.target.value);
-                setSearchQuery(event.target.value);
+                // setSearchQuery(event.target.value);
               }}
             ></input>
             <p>Creator</p>
@@ -153,6 +247,7 @@ function App() {
               id="creator"
               name="creator"
               placeholder="Frank Ocean"
+              value={creator}
               onChange={(event) => {
                 setCreator(event.target.value);
               }}
@@ -163,6 +258,7 @@ function App() {
               id="rating"
               name="rating"
               placeholder="9.15"
+              value={rating}
               onChange={(event) => setRating(event.target.value)}
             ></input>
             <div id="reviewBottom">
@@ -171,18 +267,70 @@ function App() {
                 id="submit-button"
                 onClick={() => {
                   if (loggedIn) {
-                    createReview(title, creator, rating);
+                    if ((title !== "") & (creator !== "") & (rating !== "")) {
+                      if (selectedImage.size < maxImageSizeBytes) {
+                        createReview(title, creator, rating, medium);
+                        console.log(selectedImage);
+                        storeImage(selectedImage, title, creator);
+                        setTitle("");
+                        setCreator("");
+                        setRating(0);
+                        window.alert("Review Submitted!");
+                      } else {
+                        window.alert(
+                          "Image file too big. Must be under: " +
+                            maxImageSizeBytes / 1000000 +
+                            "MB"
+                        );
+                      }
+                    } else {
+                      window.alert(
+                        "Fill out all information before submitting"
+                      );
+                    }
                   } else {
                     window.alert(
                       "Not logged in. Please sign in to submit a review"
                     );
-                    // Must be signed in message
                   }
                 }}
               >
                 Submit Review!
               </button>
             </div>
+          </div>
+        </div>
+        <div className="contentContainer">
+          <div className="sideBar">
+            <input
+              id="searchBar"
+              type="text"
+              placeholder="Enter user name or id"
+            />
+            <button id="searchButton">
+              <img id="searchImage" src="images/search-icon.png" />
+            </button>
+            {/* getUsers().map((user) => {
+            <div>{user.name}</div>;
+          }) */}
+          </div>
+          <div className="carouselContainer">
+            <Carousel>
+              <div>
+                <img src={displayReviews[0].image} />
+                <p className="legend">
+                  {displayReviews[0].title} - {displayReviews[0].rating}
+                </p>
+              </div>
+              <div>
+                <img src={displayReviews[1].image} />
+                <p className="legend">{displayReviews[1].title}</p>
+              </div>
+              <div>
+                <img src={displayReviews[2].image} />
+                <p className="legend">{displayReviews[2].title}</p>
+              </div>
+            </Carousel>
           </div>
         </div>
       </div>
--- arsha/src/firebase/config.js
+++ arsha/src/firebase/config.js
@@ -1,22 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import {
-	getFirestore,
-	collection,
-	doc,
-	setDoc,
-	getDoc,
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
 } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import slugify from "slugify";
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID,
-	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -25,46 +27,77 @@ export const auth = getAuth();
 export const provider = new GoogleAuthProvider();
 const db = getFirestore();
 
-export async function createReview(title, creator, rating) {
-	const userID = auth.currentUser.uid;
-	const reveiwDoc = doc(
-		collection(doc(collection(db, "users"), userID), "reviews"),
-		slugify(title).toLowerCase()
-	);
-	const data = {
-		userNum: await getCounter(),
-		userID: userID,
-		title: title,
-		creator: creator,
-		rating: rating,
-		creationDate: new Date(),
-		slug: slugify(title).toLowerCase(),
-	};
-	await setDoc(reveiwDoc, data);
-	await incrementCounter();
+export async function createReview(title, creator, rating, medium) {
+  const userID = auth.currentUser.uid;
+  const reveiwDoc = doc(
+    collection(doc(collection(db, "users"), userID), "reviews"),
+    slugify(title + "-" + creator).toLowerCase()
+  );
+  const data = {
+    userNum: await getCounter(),
+    userID: userID,
+    title: title,
+    creator: creator,
+    rating: rating,
+    medium: medium,
+    creationDate: new Date(),
+    image: await getImage(title, creator),
+    slug: slugify(title + "-" + creator).toLowerCase(),
+  };
+  await setDoc(reveiwDoc, data);
+  await incrementCounter();
 }
 
 export async function storeNewUser() {
-	//console.log(auth);
-	//console.log(getAdditionalUserInfo(auth.currentUser.uid)["_tokenresponse"].isNewUser);
+  //console.log(auth);
+  //console.log(getAdditionalUserInfo(auth.currentUser.uid)["_tokenresponse"].isNewUser);
+}
+
+export async function getUserReviews() {
+  //const userID = auth.currentUser.uid;
+  const userID = "ViazQOmE1Qc8owvrFOaXfqlFaSg1";
+  const reviewCol = collection(doc(collection(db, "users"), userID), "reviews");
+  const reviewSnapshot = await getDocs(reviewCol);
+  const reviews = [];
+  reviewSnapshot.forEach((document) => {
+    reviews.push(document.data());
+  });
+  return reviews;
+}
+
+export async function getImage(title, creator) {
+	const storage = getStorage();
+	return getDownloadURL(ref(storage, "art-images/" + slugify(title + "-" + creator).toLowerCase()))
+}
+
+export async function storeImage(image, title, creator) {
+  const storage = getStorage();
+  const imageRef = ref(
+    storage,
+    "art-images/" + slugify(title + "-" + creator).toLowerCase()
+  );
+  var file = new File([image], slugify(title + "-" + creator).toLowerCase(), {
+    type: "image/jpeg",
+  });
+  uploadBytes(imageRef, file);
 }
 
 export async function getCounter() {
-	const infoRef = doc(collection(db, "users"), "info");
-	const infoSnap = await getDoc(infoRef);
-	if (!infoSnap.exists()) {
-		console.log("No info document!");
-	}
-	return infoSnap.data().counter;
+  const infoRef = doc(collection(db, "users"), "info");
+  const infoSnap = await getDoc(infoRef);
+  if (!infoSnap.exists()) {
+    console.log("No info document!");
+  }
+  return infoSnap.data().counter;
 }
 
 export async function incrementCounter() {
-	const infoRef = doc(collection(db, "users"), "info");
-	const infoSnap = await getDoc(infoRef);
-	if (!infoSnap.exists()) {
-		console.log("No info document!");
-	}
-	setDoc(infoRef, { counter: infoSnap.data().counter++ });
+  const infoRef = doc(collection(db, "users"), "info");
+  const infoSnap = await getDoc(infoRef);
+  if (!infoSnap.exists()) {
+    console.log("No info document!");
+  }
+  setDoc(infoRef, { counter: infoSnap.data().counter++ });
 }
 
 export async function getUsers() {}
